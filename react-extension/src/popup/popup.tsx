b99879270ae4aa723adde22cfbbedf84b376a157
RafaelFCarneiro <rafaferreiracarneiro@gmail.com>
2023-09-22 12:23:39 -0300

    build(popup): basic set up for popup page

---
+++ react-extension/src/popup/popup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+const root = createRoot(container);
+root.render(<p>Hello World</p>);
---
+++ react-extension/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: path.resolve('src/popup/popup.tsx')
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/static'),
+        to: path.resolve('dist')
+      }]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'React Extension',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    })
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', 'js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}; 
