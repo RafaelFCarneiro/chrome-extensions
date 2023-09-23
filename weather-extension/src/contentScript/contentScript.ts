e3aa7943745adc9b8539c0e1dec872ba263c7012
RafaelFCarneiro <rafaferreiracarneiro@gmail.com>
2023-09-23 14:17:49 -0300

    build(weather): extension startup

---
+++ weather-extension/src/contentScript/contentScript.ts
@@ -0,0 +1,2 @@
+// TODO: content script
+chrome.runtime.sendMessage('From the content script', console.log);
---
+++ weather-extension/src/popup/popup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './popup.css';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container);
+
+const test = <img src='icon.png' />;
+root.render(test);
