723e2dcb54b6f9830ce2e06a6b6715419741f79d
RafaelFCarneiro <rafaferreiracarneiro@gmail.com>
2023-09-24 14:47:09 -0300

    build(weather): fix extension resolve

--- weather-extension/webpack.common.js
+++ weather-extension/webpack.common.js
@@ -44,7 +44,7 @@ module.exports = {
     ...getHtmlPlugins(["popup", "options"]),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", "js"],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "[name].js",
